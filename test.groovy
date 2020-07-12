8199013c552f02fca624d8493a19729940690025
Qinghao Shi <Qinghao.Shi@arm.com>
2020-07-12 23:00:27 +0100

    fix old

--- test.groovy
+++ test.groovy
@@ -30,24 +30,36 @@ pipeline {
 
                     stage("stage 1") {
                         stage('stage1 step1') {
+                            steps {
                                 echo "I am in stage 1 step 1"
+                            }
                         }
                         stage('stage1 step2') {
-                            echo "I am in stage 1 step 2"
+                            steps {
+                                echo "I am in stage 1 step 2"
+                            }
                         }
                         stage('stage1 step3') {
-                            echo "I am in stage 1 step 3"
+                            steps {
+                                echo "I am in stage 1 step 3"
+                            }
                         }
                     }
                     stage("stage 2") {
                         stage('stage2 step1') {
-                            echo "I am in stage 2 step 1"
-                        };
+                            steps {
+                                echo "I am in stage 2 step 1"
+                            }
+                        }
                         stage('stage2 step2') {
-                            echo "I am in stage 2 step 2"
-                        };
+                            steps {
+                                echo "I am in stage 2 step 2"
+                            }
+                        }
                         stage('stage2 step3') {
-                            echo "I am in stage 2 step 3"
+                            steps {
+                                echo "I am in stage 2 step 3"
+                            }
                         }
                     }
             }
