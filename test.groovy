dc341f2031dba86061da37c306c6526b3a61e6e3
Qinghao Shi <Qinghao.Shi@arm.com>
2020-07-12 17:44:57 +0100

    test

--- test.groovy
+++ test.groovy
@@ -30,11 +30,21 @@ pipeline {
                         //}
                         parallel (
                             "stage 1" : {
-                                echo "I am in stage 1"
+                                stage('stage1 step1') {
+                                    echo "I am in stage 1 step 1"
+                                }
+                                stage('stage1 step2') {
+                                    echo "I am in stage 1 step 2"
+                                }
                             } ,
                             
                             "stage 2" : {
-                                echo "I am in stage 1"
+                                stage('stage2 step1') {
+                                    echo "I am in stage 2 step 1"
+                                }
+                                stage('stage2 step2') {
+                                    echo "I am in stage 2 step 2"
+                                }
                             }
                         )
                         
