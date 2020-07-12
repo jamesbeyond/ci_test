d37be4e4d4c239155f78d752117d12a1d0ccb01e
Qinghao Shi <Qinghao.Shi@arm.com>
2020-07-12 17:17:48 +0100

    test

--- test.groovy
+++ test.groovy
@@ -26,7 +26,7 @@ pipeline {
                 script {
                         def apps = [:]
                         for (app in params.APPS.tokenize()) {
-                            apps[ "${app}" ] = performDeploymentStages(node, app)
+                            apps[ "${app}" ] = performDeploymentStages( app)
                         }
                         parallel apps
                         
