c85470942a9f1932f15f08b65814c48089b1956f
Qinghao Shi <Qinghao.Shi@arm.com>
2020-07-13 00:45:53 +0100

    working new

--- test.groovy
+++ test.groovy
@@ -12,13 +12,17 @@ def performDeploymentStages(String node, String app) {
 
 def createStage(sname) {
     stage("${sname} step1") {
-        if ( sname == "stage 1" ) {
-            echo "I am in ${sname} step 1"
+        step {
+            if ( sname == "stage 1" ) {
+                echo "I am in ${sname} step 1"
+            }
         }
     };
     stage("${sname} step2") {
-        if ( sname == "stage 2" )  {
-            echo "I am in ${sname} step 2"
+        step {
+            if ( sname == "stage 2" )  {
+                echo "I am in ${sname} step 2"
+            }
         }
     }
 }
