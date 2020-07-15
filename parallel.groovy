1d08f685bb14d1997939d2efe988f1554a9004bb
Qinghao Shi <Qinghao.Shi@arm.com>
2020-07-15 23:03:57 +0100

    fail

--- parallel.groovy
+++ parallel.groovy
@@ -66,7 +66,7 @@ pipeline {
             steps {
                 script {
                     echo "This is Gap"
-                    currentBuild.result='UNSTABLE'
+                    sh('false')
                 }
             }
         }
