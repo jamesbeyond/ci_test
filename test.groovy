0b02de6b6015e350b8a0fd82991a074b70476270
Qinghao Shi <Qinghao.Shi@arm.com>
2020-07-16 00:42:43 +0100

    fail

--- test.groovy
+++ test.groovy
@@ -56,9 +56,9 @@ pipeline {
             steps {
                 echo "This is the parallel"
                 parallel {
-                        stage("stage 1"){steps { script {createStage("stage 1")}}}
-                        stage("stage 2"){steps { script {createStage("stage 2")}}}
-                        stage("stage 3"){steps { script {createStage("stage 3")}}}
+                        stage("stage 1"){script {createStage("stage 1")}}
+                        stage("stage 2"){script {createStage("stage 2")}}
+                        stage("stage 3"){script {createStage("stage 3")}}
                 }
             }
         }
